324035ad9030a41624d7108b6474fcc0b4eb80f3
John Aquino <johnaquino@outlook.com>
2023-12-30 10:22:11 -0500

    Remove commented out NavBar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        {/* <NavBar /> */}
       </body>
     </html>
   )
